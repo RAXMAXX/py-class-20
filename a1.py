4af60ddf101062a9fd673697c35580079c5f6f18
RAXMAXX <rayyanhussain.kgp@gmail.com>
2026-04-25 15:46:49 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,12 @@
+tuple1=(1,3,6,9,12,45)
+print("tuple1=",tuple1)
+
+t1 = (3,4,5,6)
+final = tuple1 + t1
+print(final)
+
+print("count=", final.count(3))
+
+tuple2 = (1,2.3,4,5,6,7,8,9)
+s= tuple2[3:5] 
+print(s)
